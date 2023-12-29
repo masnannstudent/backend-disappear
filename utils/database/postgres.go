a83b8ce9bd9b9e485d453ae84bdd6bf17f5072c2
Anan Widianto <anan.widi1@gmail.com>
2023-12-29 14:55:34 +0700

    bugfix: error connection db

--- utils/database/postgres.go
+++ utils/database/postgres.go
@@ -5,14 +5,16 @@ import (
 	"github.com/capstone-kelompok-7/backend-disappear/config"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"os"
 )
 
 func InitPGSDatabase(config config.Config) *gorm.DB {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable",
-		config.DBHost, config.DBUser, config.DBPass, config.DBName, config.DBPort)
+	//dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable",
+	//	config.DBHost, config.DBUser, config.DBPass, config.DBName, config.DBPort)
 
+	connection := os.Getenv("DATABASE_URL")
 	// Open a connection to the database
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(connection), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect to database")
 	}
